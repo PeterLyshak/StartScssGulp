a8cf87517c1ec4089333c40f9cdc43036916381c
pasha.khoroz <pashakotov999@gmail.com>
2020-04-21 18:55:46 +0300

    Fix Popup

--- assets/js/main.js
+++ assets/js/main.js
@@ -43,13 +43,13 @@ function closeModal(hrefModal) {
 			bodyScrollLock.clearAllBodyScrollLocks();
 		}).trigger('afterCloseModal');
 
-		bodyScrollLock.enableBodyScroll($(hrefModal)[0]);
+		bodyScrollLock.clearAllBodyScrollLocks();
 	}, 200);
 }
 
 $(document).keydown(function(event) { 
 	if (event.keyCode == 27) { 
-		closeModals();
+		closeAllModals();
 	}
 });
 
